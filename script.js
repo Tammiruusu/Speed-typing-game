3d96f0897d594b52ef7953124734c1614fa15eb3
Tiia <tiiu_maria@hotmail.com>
2026-08-30 20:21:02 +0300

    Scripti tarkistaa oikein ja väärin kirjoituksen toimivasti

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const RANDOM_QUOTE_API_URL = 'https://dummyjson.com/quotes/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if(character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+        } else if(character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+        }
+    })
+})
+
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
         .then(response => response.json())
@@ -10,7 +28,12 @@ function getRandomQuote() {
 
 async function renderNewQuote() {
     const quote = await getRandomQuote();
-    quoteDisplayElement.innerText = quote;
+    quoteDisplayElement.innerText = '';
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+            characterSpan.innerText = character
+            quoteDisplayElement.appendChild(characterSpan)
+    })
     quoteInputElement.value = null;
 }
 
